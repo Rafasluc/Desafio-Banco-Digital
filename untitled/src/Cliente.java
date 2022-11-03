d5702abb6042d9bf77fe372516516ca7766a0c10
Rafasluc <rafael-lucena@live.com>
2022-11-03 20:56:17 -0300

    Banco Digital Basico com Sacar, Transferir e Depositar.

---
+++ untitled/src/Cliente.java
@@ -0,0 +1,14 @@
+public class Cliente {
+
+    // Variáveis
+    private String nome;
+
+    //Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
